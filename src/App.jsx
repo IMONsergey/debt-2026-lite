9414e70aafb8b74337d47d9716137e6ce206910c
Sergei I'MON <jabba.person@gmail.com>
2026-08-17 21:19:32 +0300

    Использовать исходные ассеты первого экрана

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const content = {
       routeHref: 'https://yandex.ru/maps/?text=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C%208%D0%90%D1%8110',
       contactLabel: 'Контакты для связи',
       contactEmail: 'redchief@rvzrus.ru',
-      organizersLabel: 'Организаторы',
+      organizersLabel: 'ОРГАНИЗАТОРЫ',
       organizersImage: assetUrl('assets/icons/organizers.svg'),
     },
   },
