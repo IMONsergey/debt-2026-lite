a62b02e0c0346fdbe03db0fdae47b7707197a398
Sergei I'MON <jabba.person@gmail.com>
2026-09-14 10:33:09 +0300

    Switch FIELD concept application entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { HeroRayColumnPortal } from './components/HeroRayColumnPortal.jsx';
-import './styles/global.css';
-import './styles/site-fidelity.css';
-import './styles/public-artwork.css';
-import './styles/hero-mobile-grid-fix.css';
-import './styles/hero-raycolumn-experiment.css';
-import './styles/ui-system.css';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <HeroRayColumnPortal />
-  </React.StrictMode>,
-);
+import React from 'react';import{createRoot}from'react-dom/client';import App from './App.jsx';import './styles/navigation-tokens.css';import './styles/navigation.css';import './styles/field.css';createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
